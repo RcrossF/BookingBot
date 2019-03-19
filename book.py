3fcc8d0ca5db1f73624e7a96beedfbcf0ffb1c9a
BigNutter6 <finnmorin@gmail.com>
2019-03-18 21:16:51 -0700

    Book with room preference

--- book.py
+++ book.py
@@ -3,24 +3,28 @@ import json
 import sys
 import lxml.html as lh
 from lxml import etree
-from datetime import datetime as dt
+import datetime as dt
 
-year = dt.now().year
-month = dt.now().month
-day = dt.now().day + 1
+today = dt.date.today() + dt.timedelta(days=7) #Get 1 week in the future
+year = today.year
+month = today.month
+day = today.day
 area = 1
 urlBase = "https://webapp.library.uvic.ca/studyrooms/"
 header={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'}
 startHour = 11
-startMin = 30
+startMin = 00
 endHour = 13
 endMin = 30
-roompref = []
+roompref = [15,14,13,12,16,11,10,9,8]
 try:
-    login = json.load("/login.json")
+    with open('login.json') as f:
+        login = json.load(f)
 except:
     print("Error loading file")
-    sys.exit()
+    sys.exit(0)
+
+
 
 #Scrapes the Uvic url provided and returns an array of dictionaries containing cells that are available for booking(because of the headers row and col indexing starts at 1)
 def scrape():      
@@ -59,18 +63,21 @@ def scrape():
                 totalIterator+=1
             j+=1
             
-                    
+        for item in col: #convert data to ints so they can be sorted
+            item['hr'] = int(item['hr'])
+            item['min'] = int(item['min'])  
+            item['room'] = int(item['room'])        
     return col
 
 
-#Books a slot for the given time(String. Possible values: 30min, 1hr, 90min, 2hr), good luck testing this before uvic rate limits you
+#Books a slot for the given time period(String, Possible values: 30min, 1hr, 90min, 2hr). Slot is a dict of the available room
 def book(slot, period):
    # url = urlBase + "edit_entry_handler.php?day={0}&month={1}&year={2}&room={3}&hour={4}&minute={5}".format(day,month,year,slot['room'],slot['hr'],slot['min'])
     url = urlBase + "edit_entry_handler.php"
     values = {'day': day,
             'month': month,
             'year': year,
-            'name':'StudyBois',
+            'name':'Literature Lads',
             'hour':slot['hr'],
             'minute':slot['min'],
             'duration': period,
@@ -81,8 +88,9 @@ def book(slot, period):
             'create_by':''} #https://github.com/SavioAlp for the correct post data
 
     response = requests.post(url,values,headers=header)
-    print(response.content)
 
+def sortList(list,reverse):
+    return sorted(good, key=lambda k: k['room'], reverse=reverse)
 
 available = scrape()
 good = []
@@ -95,8 +103,27 @@ for a in available: # Filter rooms by times we want
 
      elif int(a['hr']) == endHour and int(a['min']) <= endMin:
         good.append(a)
-        
-#print(good)
 
 
-book(good[0],"30min")
+
+good = sortList(good,True)
+
+i=0
+j=0
+while(True): #Book better rooms first
+    if(good[i]['room'] < roompref[j]):
+        if i == len(good)-1:
+            print("Room {0} full, trying room {1}".format(roompref[j],roompref[j+1]))
+            i=0
+            j+=1
+            continue
+        else:   
+            i+=1
+            continue
+    elif(good[i]['room'] == roompref[j]):
+        #book(good[i],'30min')
+        print("Booked room {0}".format(good[i]['room']))
+        break
+
+#if good:    book(good[0],"30min")
+#else:   print("No rooms")
