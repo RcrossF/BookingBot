b0e053a32966313e42b96f66f2d60ca548c99846
RcrossF <finnmorin@gmail.com>
2021-09-14 21:04:11 +0000

    Book a full week in advance now

--- room_snag.py
+++ room_snag.py
@@ -1,6 +1,5 @@
 '''
-Run this at midnight to secure those primo rooms, this books 6 days in advance to play it extremely safe and give real people a chance to book the rooms.
-If bots become a problem then running this 7 days in advance will probably be needed
+Run this after the library closes for the day. Rooms are booked 1 week in advance
 '''
 from bookV2 import book
 import datetime as dt
@@ -8,7 +7,7 @@ import sys
 import json
 import pytz
 
-target_day = dt.datetime.now(pytz.timezone('US/Pacific')).date() - dt.timedelta(days=1)
+target_day = dt.datetime.now(pytz.timezone('US/Pacific')).date()
 weekday = target_day.strftime('%A')
 
 try:
@@ -30,7 +29,7 @@ for times in booking_times:
     end = dt.timedelta(hours=end_h, minutes=end_m)
 
     for i in range(1,4): # Run through all the floors
-        result = book(6, start.total_seconds(), end.total_seconds())
+        result = book(7, start.total_seconds(), end.total_seconds())
         if result == "No rooms found":
             if i == 3: #If we're on the last floor and nothing was found
                 print(f"Nothing found from {start_str}-{end_str}")
