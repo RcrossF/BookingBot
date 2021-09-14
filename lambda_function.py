d59101e1c153220594e373f1f17cc28d6c278f24
RcrossF <finnmorin@gmail.com>
2021-09-14 22:44:16 +0000

    Complete lambda handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def main(event, context):
     import room_snag
+
+if __name__ == "__main__":
+    main()
